cc7012276971ba346f46cf2615d7afab9cdd9d8b
zhengzhenxing <zhengzhx@haier.com>
2017-11-08 14:24:59 +0800

    0.2.0

---
+++ UHCategory/Classes/UIView+UICrashSafety.h
@@ -0,0 +1,12 @@
+//
+//  UIView+UICrashSafety.h
+//  Pods-UHCategory_Tests
+//
+//  Created by 郑振兴 on 2017/11/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (UICrashSafety)
+
+@end
---
+++ UHCategory/Classes/UIView+UICrashSafety.m
@@ -0,0 +1,12 @@
+//
+//  UIView+UICrashSafety.m
+//  Pods-UHCategory_Tests
+//
+//  Created by 郑振兴 on 2017/11/8.
+//
+
+#import "UIView+UICrashSafety.h"
+
+@implementation UIView (UICrashSafety)
+
+@end
